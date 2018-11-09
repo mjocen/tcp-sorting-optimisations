765ed6e1b9ea30c4a81801541cf8bd431ca4a9ee
mjocen <30681814+mjocen@users.noreply.github.com>
2018-11-10 10:42:36 +1100

    added optimised algorithm but it's just rearranging the default one. accuracy is still bad but improved time by 2% at most but at random turnout.

--- algorithms/optimised.js
+++ algorithms/optimised.js
@@ -1,7 +1,68 @@
+const constants = require("../constants");
+
+
+function calcDate(taskA, taskB) {
+    let dca = parseInt(taskA.date_completion_due);
+    let dcb = parseInt(taskB.date_completion_due);
+    if (dca === dcb)
+        return 0;
+    return (dca < dcb ? -1 : 1);
+}
+    
+function calcPriority(taskA, taskB) {
+    let dca = parseInt(taskA.priority);
+    let dcb = parseInt(taskB.priority);
+    if (dca === dcb)
+        return 0;
+    return (dca < dcb ? -1 : 1);
+}
+
 function optimisedSort(tasks) {
 
-	//Add your sorting code here...
-	return tasks;
+    //Add your sorting code here...
+    if (tasks.length < 2)
+		return tasks;
+    
+    let allTasks = [];
+    let awaitingTasks = [];
+    let remainingTasks = [];
+    let grouped = {};
+    
+    
+    //Separate tasks according to status
+    for (let task of tasks) {
+        if (parseInt(task.status) == constants.TASK_STATUS_IN_PROGRESS) {
+            allTasks.push(task)
+        } else {
+            if (! grouped.hasOwnProperty(task.priority))
+                grouped[task.priority] = [];
+            grouped[task.priority].push(task);
+        }
+    }
+
+    let groupedArr = [];
+    for (let group in grouped)
+        groupedArr.push(grouped[group]);
+    
+    for (let group of groupedArr) {
+        group.sort(calcDate);
+        
+        for (let task of group) {
+            if (parseInt(task.status) == constants.TASK_STATUS_GROUP_WAIT)
+                awaitingTasks.push(task);
+            else
+                remainingTasks.push(task);
+        }
+    }
+
+    for (let task of awaitingTasks)
+        allTasks.push(task);
+    
+    for (let task of remainingTasks)
+        allTasks.push(task);
+
+	return allTasks;
 }
+
 
 module.exports = optimisedSort;
